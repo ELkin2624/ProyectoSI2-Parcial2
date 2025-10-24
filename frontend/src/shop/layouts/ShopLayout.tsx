39509fbcedaf7cf19d84f754360394bcb0ca9456
unknown <alan.melgar.f@gmail.com>
2025-10-24 19:46:08 -0400

    diseño homepage

--- frontend/src/shop/layouts/ShopLayout.tsx
+++ frontend/src/shop/layouts/ShopLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router"
+import Header from "../components/CustomHeader"
+import { CustomFooter } from "../components/CustomFooter"
 
 export const ShopLayout = () => {
     return (
-        <div>
-            ShopLayout
+        <div className="min-h-screen bg-background">
+            <Header />
             <Outlet />
+            <CustomFooter />
         </div>
     )
 }
